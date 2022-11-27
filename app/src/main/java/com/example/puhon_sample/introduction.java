dc3b55966172e763389b908844e77d27992f400e
Alyssa Arias <alyssaarias27@gmail.com>
2022-11-27 22:05:26 +0800

    revised login screen and added diff layouts

--- app/src/main/java/com/example/puhon_sample/introduction.java
+++ app/src/main/java/com/example/puhon_sample/introduction.java
@@ -11,7 +11,6 @@ public class introduction extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_introduction);
 
         Button button3 = findViewById(R.id.btn_get);

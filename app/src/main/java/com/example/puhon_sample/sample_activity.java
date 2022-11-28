afb94aeba7ef578b0fb963f49e9e3d0f4a115b98
Janika <jnkescolano@gmail.com>
2022-11-28 18:06:35 +0800

    moodscape menu (button design to be followed)

---
+++ app/src/main/java/com/example/puhon_sample/sample_activity.java
@@ -0,0 +1,14 @@
+package com.example.puhon_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sample_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}

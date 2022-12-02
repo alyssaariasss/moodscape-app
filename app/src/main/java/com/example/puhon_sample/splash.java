38f17fd3da0145686d0301be2f85b35294622caa
Alyssa Arias <alyssaarias27@gmail.com>
2022-12-02 18:42:43 +0800

    redirect to menu if user is logged in

--- app/src/main/java/com/example/puhon_sample/splash.java
+++ app/src/main/java/com/example/puhon_sample/splash.java
@@ -1,23 +1,35 @@
 package com.example.puhon_sample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 public class splash extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
+        mAuth = FirebaseAuth.getInstance();
+
         int SPLASH_SCREEN = 6000;
         new Handler().postDelayed(() -> {
-            Intent intent= new Intent(splash.this, MainActivity.class);
-            startActivity(intent);
+            if (mAuth.getCurrentUser() != null) {
+                Intent intent= new Intent(splash.this, menu.class);
+                startActivity(intent);
+            } else {
+                Intent intent= new Intent(splash.this, MainActivity.class);
+                startActivity(intent);
+            }
             finish();
         }, SPLASH_SCREEN);
 

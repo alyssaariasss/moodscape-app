d40bcc3fc4a129890ee6521fae2257fa822e910d
Alyssa Arias <alyssaarias27@gmail.com>
2022-12-20 16:25:39 +0800

    storing mood + fixed fragment layout of summary

--- app/src/main/java/com/example/puhon_sample/UserMoods.java
+++ app/src/main/java/com/example/puhon_sample/UserMoods.java
@@ -2,16 +2,30 @@ package com.example.puhon_sample;
 
 public class UserMoods {
 
-    private String mood;
+    private String mood, date;
 
     public UserMoods(){
 
     }
 
-    public UserMoods(String mood) {this.mood = mood; }
+    public UserMoods(String mood, String date) {
+        this.mood = mood;
+        this.date = date;
+    }
 
-    public void setMood(String mood) {this.mood = mood; }
+    public String getMood() {
+        return mood;
+    }
 
-    public String getMood() { return mood; }
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }

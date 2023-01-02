d9a0295de446f5e49ff785c46943fd3b8a0077a6
Alyssa Arias <alyssaarias27@gmail.com>
2023-01-02 21:53:20 +0800

    added goal layouts

--- app/src/main/java/com/example/puhon_sample/goals.java
+++ app/src/main/java/com/example/puhon_sample/goals.java
@@ -1,19 +1,37 @@
 package com.example.puhon_sample;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class goals extends AppCompatActivity {
 
-    TextView goals_Today, goals_History;
+    TextView goals_Today, goals_History, goals_greetings;
     private int selectedTabNumber = 1;
+
+    FirebaseAuth fAuth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    String id;
+    User userprofile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +40,43 @@ public class goals extends AppCompatActivity {
 
         goals_Today = findViewById(R.id.goals_Today);
         goals_History = findViewById(R.id.goals_History);
+        goals_greetings = findViewById(R.id.goals_greetings);
+
+        GreetUser();
+
+        // NavBar Buttons
+
+        ImageButton btn_home = findViewById(R.id.nav_home);
+
+        btn_home.setOnClickListener(v -> {
+
+            Intent intent = new Intent(this, menu.class);
+            startActivity(intent);
+        });
+
+        ImageButton btn_info = findViewById(R.id.nav_about_mood);
+
+        btn_info.setOnClickListener(v -> {
+
+            Intent intent = new Intent(this, BreakScreen1.class);
+            startActivity(intent);
+        });
+
+        ImageButton btn_progress = findViewById(R.id.nav_progress);
+
+        btn_progress.setOnClickListener(v -> {
+
+            Intent intent = new Intent(this, summary.class);
+            startActivity(intent);
+        });
+
+        ImageButton btn_settings = findViewById(R.id.nav_settings);
+
+        btn_settings.setOnClickListener(v -> {
+
+            Intent intent = new Intent(this, settings.class);
+            startActivity(intent);
+        });
 
         // selecting first fragment by default
         if (savedInstanceState == null) {
@@ -46,6 +101,32 @@ public class goals extends AppCompatActivity {
 
                 selectTab(2);
 
+            }
+        });
+    }
+
+    // Retrieves user's first name
+    private void GreetUser() {
+        fAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        FirebaseUser user = fAuth.getCurrentUser();
+        assert user != null;
+        id = user.getUid();
+        reference = database.getReference().child("users").child(id);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                userprofile = snapshot.getValue(User.class);
+                assert userprofile != null;
+                goals_greetings.setText(String.format("Hello, %s!", userprofile.getUserFirstName()));
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(goals.this, "Something is wrong.", Toast.LENGTH_SHORT).show();
             }
         });
     }
